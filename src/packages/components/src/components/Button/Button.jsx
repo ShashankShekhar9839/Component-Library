ed00c4c836984049d64eda48cdde76c7fd2ae723
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-07-23 00:21:46 +0530

    added more functionalities to btn

--- src/packages/components/src/components/Button/Button.jsx
+++ src/packages/components/src/components/Button/Button.jsx
@@ -1,14 +1,58 @@
 import React from "react";
 import "shashank-monoatomic-css/dist/styles.css";
+import PropTypes from "prop-types";
+
+const propTypes = {
+  label: PropTypes.node,
+  type: PropTypes.oneOf(["Primary", "Secondary", "Tertiary"]),
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
+  isBlock: PropTypes.bool,
+  onclick: PropTypes.func,
+};
 
 const Button = (props) => {
-  const { label, type, isBlock } = props;
+  const { label, isBlock, type = "Tertiary", size = 'lg' } = props;
 
-  const commonBtnClasses = `${isBlock ? "dis-block" : "dis-inline-block" }`;
-  const primaryBtnClasses = `bg-teal h-6 p-2 col-white`;
+  const commonBtnClasses = `${isBlock ? "dis-block" : "dis-inline-block"} flex flex-align-items-center justify-content-center`;
+
+  const getBtnClasses = (type) => {
+    switch (type) {
+      case "Primary":
+        return `bg-teal  p-2 text-white`;
+      case "Secondary":
+        return `bg-red  p-2 text-white`;
+      case "Tertiary":
+        return `bg-black  p-2 text-white`;
+    }
+  };
+
+  const getBtnSizeClasses = (size) => {
+    switch(size) {
+      case 'sm': 
+      return `h-5`;
+      case 'md' : 
+      return `h-6`;
+      case 'lg': 
+      return `h-7`;
+    }
+  }
+
+  const handleClick = () => {
+    const { onClick } = props;
+    if (onClick) {
+      onClick();
+    }
+  };
   return (
-    <div className={`${primaryBtnClasses} ${commonBtnClasses}`} style={{textAlign: 'center'}}>{label}</div>
+    <div
+      className={`${getBtnClasses(type)} ${getBtnSizeClasses(size)} ${commonBtnClasses}`}
+      style={{ textAlign: "center" }}
+      onClick={handleClick}
+    >
+      {label}
+    </div>
   );
 };
 
+Button.propTypes = propTypes;
 export default Button;
