9ab08b0a4a90a54b88217b369ecf8f681e5f15cd
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-07-24 23:47:48 +0530

    Update Button.stories.js

--- src/packages/components/src/stories/Button.stories.js
+++ src/packages/components/src/stories/Button.stories.js
@@ -1,28 +1,25 @@
-
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Component, useRef } from "react";
 import Button from "../components/Button/Button";
-import 'shashank-monoatomic-css/dist/styles.css';
+import "shashank-monoatomic-css/dist/styles.css";
 
 export default {
-    title: 'Example/Button',
-    component : Button,
-}
+  title: "Example/Button",
+  component: Button,
+};
 
-const Template = (args) =>{
+const Template = (args) => {
   let buttonRef = useRef(null);
   useEffect(() => {
-   if(buttonRef.current) {
-    console.log(buttonRef.current);
-   }
-  })
-  return <Button {...args} ref={buttonRef}/>;
-
-} 
-
+    if (buttonRef.current) {
+      console.log(buttonRef.current);
+    }
+  });
+  return <Button {...args} ref={buttonRef} />;
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
-  label: 'Primary Button',
+  label: "Primary Button",
   isBlock: true,
 };
